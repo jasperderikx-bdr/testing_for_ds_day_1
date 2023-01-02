024f4722a7bb670dee6b5b1d8762c599eb136d05
Jasper <jasper.derikx@politie.nl>
2023-01-02 21:09:57 +0100

    add answers for fixtures and parametrize

--- tests/test_2_fixtures.py
+++ tests/test_2_fixtures.py
@@ -1,6 +1,9 @@
 from typing import Generator, List
 
 import pytest
+from _pytest.fixtures import FixtureRequest
+
+from hanoi.basics import Position
 
 # -- Exercise 1 --
 # Make test_fixture_sequence() pass. The test may only depend on 2 fixtures, but you can modify the dependencies of the
@@ -64,11 +67,35 @@ def rotate_3(b: List, t: None) -> None:
 def test_fixture_sequence(b: List, e: None) -> None:  # This test may depend on just 1 fixture, besides the fixture "b".
     assert b == ["v", "a", "n", "t", "a", "g", "e"]
 
+
 # -- Exercise 2 --
 # What happens if a fixture with a broader scope, depends on a fixture with a narrower scope?
+@pytest.fixture(scope="class")
+def narrower_fixture() -> Generator:
+    yield "narrow"
+
+
+@pytest.fixture(scope="module")
+def broader_fixture(narrower_fixture: str) -> Generator:
+    yield f"broader depends on {narrower_fixture}"
+
+
+@pytest.mark.xfail(reason="Broader fixture requests narrower fixture.", run=True, strict=True)
+def test_fixture_scopes(broader_fixture: str) -> None:
+    assert type(broader_fixture) == str
 
 
 # -- Exercise 3 --
 # It's possible to pass data from a test to a fixture, so the result of a fixture depends on data in the test. Let's
 # create a fixture that returns the start position of the puzzle and use it in a test. The fixture depends on the size
 # of the puzzle. Documentation https://docs.pytest.org/en/6.2.x/fixture.html#using-markers-to-pass-data-to-fixtures.
+# Finally, make sure to register your mark.
+@pytest.fixture()
+def start_position(request: FixtureRequest) -> Position:
+    number_of_disks = request.node.get_closest_marker("number_of_disks").args[0]
+    return Position(number_of_disks * "a")
+
+
+@pytest.mark.number_of_disks(3)
+def test_start_position(start_position: Position) -> None:
+    assert start_position == Position("aaa")
--- tests/test_3_parametrize.py
+++ tests/test_3_parametrize.py
@@ -1,13 +1,21 @@
+import pytest
+
+from hanoi.basics import number_of_positions
 
 
 # -- Exercise 1 --
 # Test the output of def number_of_positions() from hanoi/basics.py, parametrizing over number_of_disks.
+@pytest.mark.parametrize("number_of_disks, expected_number_of_positions", [(1, 3), (2, 9), (3, 27)])
+def test_number_of_positions(number_of_disks: int, expected_number_of_positions: int) -> None:
+    assert number_of_positions(number_of_disks=number_of_disks) == expected_number_of_positions
 
 
 # -- Exercise 2 --
 # What happens to the test_ids of parametrized tests? (Use ```pytest --collect-only```.)
 # Find out what "ids" does, when used in pytest.mark.parametrize(... ids= ...).
-
+@pytest.mark.parametrize("number_of_disks", [1, 2, 3], ids=["small", "medium", "large"])
+def test_parameter_with_ids(number_of_disks: int) -> None:
+    assert True
 
 # -- Exercise 3 --
 # Like exercise 1, write a test for number_of_steps_of_solution(). But now, parametrize all tests in this module at
