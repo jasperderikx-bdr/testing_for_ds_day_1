73a1d7d934d94056543d538b886aa1a817d236ef
Jasper <jasper.derikx@politie.nl>
2023-01-03 15:42:51 +0100

    add fixture exercise

--- tests/test_2_fixtures.py
+++ tests/test_2_fixtures.py
@@ -1,68 +1,29 @@
 from typing import Iterator, List
 
+import pandas as pd
 import pytest
 
-# -- Exercise 1 --
-# Make test_fixture_sequence() pass. The test may only depend on 2 fixtures, but you can modify the dependencies of the
-# following fixtures.
+
+# -- Exercise 1* --
+# If one fixture is (indirectly) requested multiple times for the same test, the value is cached. Make the test pass.
+@pytest.fixture
+def b() -> pd.DataFrame:
+    return pd.DataFrame(columns=["b"])
 
 
 @pytest.fixture
-def b() -> Iterator[List]:
-    yield []
+def d(b: pd.DataFrame) -> None:
+    b["d"] = []
 
 
 @pytest.fixture
-def v(b: List) -> None:
-    b.append("v")
+def r(b: pd.DataFrame) -> None:
+    b["r"] = []
 
 
-@pytest.fixture
-def a(b: List) -> None:
-    b.append("a")
+def test_bdr(b: pd.DataFrame, d: None, r: None) -> None:
+    assert list(b.columns) == "?"
 
-
-@pytest.fixture
-def n(b: List) -> None:
-    b.append("n")
-
-
-@pytest.fixture
-def t(b: List) -> None:
-    b.append("t")
-
-
-@pytest.fixture
-def g(b: List) -> None:
-    b.append("g")
-
-
-@pytest.fixture
-def e(b: List) -> None:
-    b.append("e")
-
-
-@pytest.fixture
-def duplicate(b: List) -> None:
-    for i in range(len(b)):
-        b.append(b[i])
-
-
-@pytest.fixture
-def rotate_1(b: List) -> None:
-    b.insert(0, b[-1])
-    b.pop()
-
-
-@pytest.fixture
-def rotate_3(b: List) -> None:
-    for i in range(3):
-        b.insert(0, b[-1])
-        b.pop()
-
-
-def test_fixture_sequence(b: List) -> None:  # This test may depend on just 1 fixture, besides the fixture "b".
-    assert b == ["v", "a", "n", "t", "a", "g", "e"]
 
 # -- Exercise 2 --
 # What happens if a fixture with a broader scope, depends on a fixture with a narrower scope?
@@ -72,3 +33,66 @@ def test_fixture_sequence(b: List) -> None:  # This test may depend on just 1 fi
 # It's possible to pass data from a test to a fixture, so the result of a fixture depends on data in the test. Let's
 # create a fixture that returns the start position of the puzzle and use it in a test. The fixture depends on the size
 # of the puzzle. Documentation https://docs.pytest.org/en/6.2.x/fixture.html#using-markers-to-pass-data-to-fixtures.
+
+
+# -- Exercise 4 --
+# Make test_vantage() pass. The test may only depend on 2 fixtures, but you can modify the dependencies of the other
+# fixtures.
+
+
+@pytest.fixture
+def x() -> Iterator[List]:
+    yield []
+
+
+@pytest.fixture
+def v(x: List) -> None:
+    x.append("v")
+
+
+@pytest.fixture
+def a(x: List) -> None:
+    x.append("a")
+
+
+@pytest.fixture
+def n(x: List) -> None:
+    x.append("n")
+
+
+@pytest.fixture
+def t(x: List) -> None:
+    x.append("t")
+
+
+@pytest.fixture
+def g(x: List) -> None:
+    x.append("g")
+
+
+@pytest.fixture
+def e(x: List) -> None:
+    x.append("e")
+
+
+@pytest.fixture
+def duplicate(x: List) -> None:
+    for i in range(len(x)):
+        x.append(x[i])
+
+
+@pytest.fixture
+def rotate_1(x: List) -> None:
+    x.insert(0, x[-1])
+    x.pop()
+
+
+@pytest.fixture
+def rotate_3(x: List) -> None:
+    for i in range(3):
+        x.insert(0, x[-1])
+        x.pop()
+
+
+def test_vantage(x: List) -> None:  # This test may depend on just 1 fixture, besides the fixture "x".
+    assert x == ["v", "a", "n", "t", "a", "g", "e"]
